6edf28a547761fd0485ee04589a4943568ba7209
fpetrakov <jakephin2015@gmail.com>
2022-05-11 13:02:45 +0300

    1.7

---
+++ 1-7-exercise.scm
@@ -0,0 +1,21 @@
+(define (square x) (* x x))
+
+(define (abs x)
+  (if (>= x 0) x (- x)))
+
+(define (good-enough? previous-guess guess)
+  (< (abs (/ (- guess previous-guess) guess)) 0.00000000001))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (sqrt-iter guess x)
+  (if (good-enough? guess (improve guess x))
+      guess
+      (sqrt-iter (improve guess x) x)))
+
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
