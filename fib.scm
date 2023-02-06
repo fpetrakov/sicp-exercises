dc6bd9daa6c86a8528c71e3812f9397a65e0f63c
fpetrakov <jakephin2015@gmail.com>
2023-02-06 11:30:29 +0300

    iterative fib

--- fib.scm
+++ fib.scm
@@ -1,5 +1,15 @@
-(define (fib n)
+(define (rec-fib n)
 	(if (< n 2)
 				n
 				(+ (fib (- n 1))
 								 (fib (- n 2)))))
+
+(define (fib n)
+	(fib-iter 1 0 n))
+
+(define (fib-iter a b count)
+	(if (= count 0)
+			b
+			(fib-iter (+ a b) a (- count 1))))
+
+(fib 8)
