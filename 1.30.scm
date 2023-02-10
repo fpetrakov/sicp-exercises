0eb4bf930c31f97042c7bb3235ca9fd1a3068889
fpetrakov <jakephin2015@gmail.com>
2023-02-10 12:50:43 +0300

    1.30

---
+++ 1.30.scm
@@ -0,0 +1,15 @@
+(define (sum term a next b)
+	(define (iter a result)
+		(if (> a b)
+				result	
+				(iter (next a) (+ result (term a)))))
+	(iter a 0))
+
+(define (inc n) (+ n 1))
+
+(define (identity x) x)
+
+(define (sum-integers a b)
+	(sum identity a inc b))
+
+(sum-integers 1 10)
