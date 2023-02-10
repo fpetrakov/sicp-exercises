2b9b47f52ecfea8ad0bf1023b682e8f8235ecf1a
fpetrakov <jakephin2015@gmail.com>
2023-02-10 13:06:43 +0300

    more 1.31

--- 1.31.scm
+++ 1.31.scm
@@ -2,13 +2,26 @@
 
 (define (identity x) x)
 
-(define (product term a next b)
+(define (product-rec term a next b)
 	(if (> a b)
 	1
 	(* (term a)
-		 (product term (next a) next b))))
+		 (product-rec term (next a) next b))))
 
-(define (factorial n)
-	(product identity 1 inc n))
+(define (factorial-rec n)
+	(product-rec identity 1 inc n))
 
-(factorial 5)
+(factorial-rec 5)
+
+(define (product-iter term a next b)
+	(define (iter a result)
+		(if (> a b)
+				result
+				(iter (next a) (* result (term a)))))
+	(iter a 1))
+
+(define (factorial-iter n)
+	(product-iter identity 1 inc n))
+
+
+(factorial-iter 5)
