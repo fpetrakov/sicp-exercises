e8fd39e55dba36c03edb2fca111123db69f0163b
fpetrakov <jakephin2015@gmail.com>
2023-02-13 17:34:50 +0300

    2.5

---
+++ 2 chapter/2.5.scm
@@ -0,0 +1,20 @@
+(define (cons a b)
+	(* (expt 2 a) (expt 3 b)))
+
+(define (car x)
+	(define (car-iter x count)
+	(if (= 0 (remainder x 2))
+			(car-iter (/ x 2) (+ 1 count))
+			count))
+	(car-iter x 0))	
+
+(define (cdr x)
+	(define (cdr-iter x count)
+	(if (= 0 (remainder x 3))
+			(cdr-iter (/ x 3) (+ 1 count))
+			count))
+	(cdr-iter x 0))
+
+(print (cons 3 18)) (newline)
+(print (car (cons 3 18))) (newline)
+(print (cdr (cons 3 18))) (newline)
