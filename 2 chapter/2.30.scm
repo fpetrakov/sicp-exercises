b88811b1ef0b3c096784e498bc9f5b71044a3e2e
fpetrakov <jakephin2015@gmail.com>
2023-02-16 21:44:02 +0300

    2.30

---
+++ 2 chapter/2.30.scm
@@ -0,0 +1,23 @@
+(define nil '())
+
+(define (scale-tree tree factor)
+	(cond ((null? tree) nil)
+				((not (pair? tree)) (* tree factor))
+				(else (cons (scale-tree (car tree) factor)
+										(scale-tree (cdr tree) factor)))))
+
+(define (square-tree tree)
+	(cond ((null? tree) nil)
+				((not (pair? tree)) (* tree tree))
+				(else (cons (square-tree (car tree))
+										(square-tree (cdr tree))))))
+
+(define (square-tree tree)
+	(map (lambda (sub-tree)
+					(if (pair? sub-tree)
+							(square-tree sub-tree)
+							(* sub-tree sub-tree)))
+				tree))
+
+(define t (list 2 (list 3 4) 5 (list 6 7)))
+(display (square-tree t)) (newline)
