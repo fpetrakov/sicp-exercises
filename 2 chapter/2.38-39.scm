50cd3ccd83923a85de7c580b3f87da712d4783a7
fpetrakov <jakephin2015@gmail.com>
2023-02-19 13:46:47 +0300

    2.39

--- 2 chapter/2.38-39.scm
+++ 2 chapter/2.38-39.scm
@@ -18,5 +18,17 @@
 (display (fold-left / 2 (list 1 2 3))) (newline)
 (display (accumulate list nil (list 1 2 3))) (newline)
 (display (fold-left list nil (list 1 2 3))) (newline)
+(newline)
 
 ; op should not depend on the order of args
+
+
+; 2.39
+(define (reverse-right sequence)
+	(accumulate (lambda (x y) (append y (cons x nil))) nil sequence))
+
+(define (reverse-left sequence)
+	(fold-left (lambda (x y) (cons y x)) nil sequence))
+
+(display (reverse-right (list 1 2 3))) (newline)
+(display (reverse-left (list 1 2 3))) (newline)
