8ef2a94d8ebdedf9943692ccb17641dc85d0d63b
fpetrakov <jakephin2015@gmail.com>
2023-02-24 13:11:10 +0300

    2.61

---
+++ 2 chapter/2.61.scm
@@ -0,0 +1,28 @@
+(define (element-of-set? x set)
+	(cond ((null? set) #f)
+				((= x (car set)) #t)
+				((< x (car set)) #f)
+				(else (element-of-set? x (cdr set)))))
+
+
+(define (intersetion-set  set1 set2)
+	(if (or (null? set1) (null? set2))
+			'()
+			(let ((x1 (car set1)) (x2 (car set2)))
+				(cond ((= x1 x2)
+							 (cons x1
+							 			 (intersection-set (cdr set1)
+										 									 (cdr set2))))
+							((< x1 x2)
+							 (intersection-set (cdr set1) set2))
+							((< x2 x1)
+							 (intersection-set (cdr set2) set1))))))
+
+(define (adjoin-set x set)
+		(cond ((null? set) (list x))
+					((= x (car set)) set)
+					((< x (car set)) (cons x set))
+					(else (cons (car set) (adjoin-set x (cdr set))))))
+	
+(display (adjoin-set 10 '(1 2 3 4 5 6 7 8 9))) (newline)
+(display (adjoin-set 5 '(1 2 3 4 6 7 8 9))) (newline)
