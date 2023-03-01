9d16e20e45f87110eb93acccf450f2b5694b1606
fpetrakov <jakephin2015@gmail.com>
2023-03-01 20:46:42 +0300

    2.78

---
+++ 2 chapter/2.78.scm
@@ -0,0 +1,14 @@
+; Tags
+(define (attach-tag type-tag contents)
+	(cond [(eq? type-tag 'scheme-number') contents]
+				[else (cons type-tag contents)]))
+
+(define (type-tag datum)
+	(cond [(number? datum) 'scheme-number]
+				[(pair? datum) (car datum)]
+				[else (error "Некорректные помеченные данные -- TYPE-TAG" datum)]))
+
+(define (contents datum)
+	(cond [(number? datum)]
+				[(pair? datum) (cdr datum)]
+				[error ("Некорректные помеченные данные -- CONTENT-TAG" datum)]))
