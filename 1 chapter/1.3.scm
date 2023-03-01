338916f14c824c5989930a597b2dff272ce57f86
fpetrakov <jakephin2015@gmail.com>
2023-03-01 20:44:38 +0300

    use square brackets

--- 1 chapter/1.3.scm
+++ 1 chapter/1.3.scm
@@ -2,14 +2,14 @@
 	(* x x))
 
 (define (sum-of-squares x y) 
-				(+ (square x) (square y)))
+				(+ [square x] [square y]))
 
 (define (sum-of-largest-squares x y z)
-				(cond ((and (> x z) (> y z)) (sum-of-squares x y))
-							((and (> x y) (> z y)) (sum-of-squares x z)) 
-							(else 								 (sum-of-squares y z)) 
+				(cond [(and (> x z) (> y z)) (sum-of-squares x y)]
+							[(and (> x y) (> z y)) (sum-of-squares x z)]
+							[else 								 (sum-of-squares y z)]
 				)
 )
 
-(sum-of-largest-squares 1 2 3)
+(display (sum-of-largest-squares 1 2 3)) (newline)
 	 
