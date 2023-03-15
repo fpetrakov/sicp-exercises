01f946b124d257cf03ac40cb490534039da04adc
fpetrakov <fpetrakov@gmail.com>
2023-03-15 13:23:19 +0300

    3.30 fix

--- 3 chapter/3.30.scm
+++ 3 chapter/3.30.scm
@@ -3,11 +3,10 @@
 (define (ripple-carry-adder a-list b-list sum-list c-out-list)
 	(let ((c-in-list (map (lambda (x) (make-wire)) (cdr a-list)))
 				(c-0 (make-wire)))
-		(map
-			full-adder
-			a-list
-			b-list
-			(append c-in-list (list c-0))
-			sum-list
-			(cons c-out-list c-in-list))
+		(map full-adder 
+				 a-list
+				 b-list
+				 (append c-in-list (list c-0))
+				 sum-list
+				 (cons c-out-list c-in-list))
 	'ok))
