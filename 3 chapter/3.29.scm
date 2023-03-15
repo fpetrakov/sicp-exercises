190c0d0a1661b3365df63f25ec322fb9045cb46e
fpetrakov <jakephin2015@gmail.com>
2023-03-15 11:59:08 +0300

    3.29

---
+++ 3 chapter/3.29.scm
@@ -0,0 +1,10 @@
+(load "./3 chapter/logical-gates.scm")
+
+(define (or-gate a1 a2 output)
+	(let ((c1 (make-wire)))
+			 ((c2 (make-wire)))
+			 ((c3 (make-wire)))
+		(inverter a1 c1)
+		(inverter a2 c2)
+		(and-gate c1 c2 c3)
+		(inverter c3 output)))
